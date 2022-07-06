372059b930178b2d3e25e77ceb900e1eb809506d
Dan Hardej <danielhardej@gmail.com>
2022-07-06 15:45:59 +0100

    Update script.py

--- build-deep-learning-models-with-tensorflow/portfolio-project-forest-cover-classification/script.py
+++ build-deep-learning-models-with-tensorflow/portfolio-project-forest-cover-classification/script.py
@@ -54,9 +54,13 @@ from tensorflow.keras.metrics import CategoricalAccuracy, AUC
 
 tf.random.set_seed(42) # for the reproducibility of results
 
+# Turn off GPU usage for tf
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2'
+
 ### Define Hyperparameters ###
-NUM_LAYERS = 6
-NUM_NEURONS = 12
+NUM_LAYERS = 4
+NUM_NEURONS = 128
 NUM_EPOCHS = 100
 BATCH_SIZE = 16
 LEARN_RATE=0.01
@@ -67,12 +71,20 @@ def process_data(data):
     labels = dataset.iloc[:,-1]
     features = dataset.iloc[:,0:-1]
     features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.33, random_state=42, stratify=y)
+
+    stan_scaler = StandardScaler()
+    features_train_normalized = stan_scaler.fit_transform(features_train)
+    features_test_normalized = stan_scaler.transform(features_test)
+
     ct = ColumnTransformer([('numeric',
                             StandardScaler(),
-                            ["Elevation", "Aspect","Slope","Horizontal_Distance_To_Hydrology","Vertical_Distance_To_Hydrology","Horizontal_Distance_To_Roadways","Hillshade_9am","Hillshade_Noon","Hillshade_3pm","Horizontal_Distance_To_Fire_Points"],
+                            ["Elevation", "Aspect","Slope","Horizontal_Distance_To_Hydrology","Vertical_Distance_To_Hydrology",
+                                "Horizontal_Distance_To_Roadways","Hillshade_9am","Hillshade_Noon","Hillshade_3pm","Horizontal_Distance_To_Fire_Points"],
                             remainder='passthrough')])
+
     features_train_scale = ct.fit_transform(features_train)
     features_test_scale = ct.transform(features_test)
+
     return features_train_scale, features_test_scale, labels_train, labels_test
 
 
@@ -83,21 +95,40 @@ def build_model(n_layers, n_neurons):
     # create hidden layers
     layer_count = 0
     for i in range(n_layers):
-        model.add(Dense(n_neurons, activation='relu'))
+        if n_neurons>8:
+            model.add(Dense(n_neurons, activation='relu'))
+        else:
+            pass
         layer_count +=1
+        n_neurons = n_neurons/2
     # create output layer
-    model.add(Dense(labels.shape[1], activation='softmax'))
+    model.add(Dense(8, activation='softmax'))
     return model
 
 
-def train_model():
-    opt = Adam(learning_rate=L_RATE)
+def train_model(features_train_scaled, labels_train):
+    opt = Adam(learning_rate=LEARN_RATE)
     my_model.compile(loss='mse', metrics='mae', optimizer=opt)
     history = my_model.fit(features_train_scaled, labels_train, epochs=N_EPOCHS, batch_size=BATCH_SIZE, verbose=1, validation_split=VAL_SPLT)
     return history
 
 def plot_results():
 
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper right')
+    plt.show()
 
 def evaluate_model():
 
@@ -107,11 +138,11 @@ def evaluate_model():
 
     loss, acc = model.evaluate(features_test_scale, labels_test, verbose=0)
 
+    print("Model summary: ")
+    print(model.summary())
+    print()
     print("Loss: ", loss)
     print("Accuracy: ", acc)
-    print()
-    print(model.summary())
-
     print()
     print("Summary: ")
     print("Number of layers: " + str(layer_count))
@@ -127,13 +158,21 @@ def evaluate_model():
     print(my_model.summary())
 
 def generate_classification_report():
-    estimate_vals = model.predict(features_test_scale)
-    estimate_vals = np.argmax(estimate_vals, axis=1)
-    true_vals = np.argmax(labels_test, axis=1)
+    prediction = np.argmax(model.predict(features_test_scale), axis=1)
+    labels_test = np.argmax(labels_test, axis=1)
+    class_names = ['Spruce/Fir', 'Lodgepole Pine', 'Ponderosa Pine', 'Cottonwood/Willow', 'Aspen', 'Douglas-fir', 'Krummholz']
     print()
-    print(classification_report(true_vals, estimate_vals))
+    print(classification_report(labels_test, prediction, target_names=class_names))
 
 
 def main():
     dataframe = pd.read_csv("cover_data.csv")
     features_train_scale, features_test_scale, labels_train, labels_test = process_data(dataframe)
+    train_model()
+    plot_results()
+    evaluate_model()
+    generate_classification_report()
+
+
+if __name__ == '__main__':
+    main()
