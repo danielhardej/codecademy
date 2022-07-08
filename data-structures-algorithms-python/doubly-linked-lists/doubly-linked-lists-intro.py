9a1853263d57e74143783eb17779423b0874e20a
Dan Hardej <danielhardej@gmail.com>
2022-07-08 20:47:02 +0100

    Update doubly-linked-lists-intro.py

--- data-structures-algorithms-python/doubly-linked-lists/doubly-linked-lists-intro.py
+++ data-structures-algorithms-python/doubly-linked-lists/doubly-linked-lists-intro.py
@@ -21,6 +21,115 @@ class Node:
 
 
 class DoublyLinkedList:
-  def __init__(self, head_node=None, tail_node=None):
-    self.head_node = head_node
-    self.tail_node = tail_node
+    def __init__(self):
+        self.head_node = None
+        self.tail_node = None
+
+    def add_to_head(self, new_value):
+        new_head = Node(new_value)
+        current_head = self.head_node
+
+        if current_head is not None:
+          current_head.set_prev_node(new_head)
+          new_head.set_next_node(current_head)
+
+        self.head_node = new_head
+
+        if self.tail_node is None:
+          self.tail_node = new_head
+
+    def add_to_tail(self, new_value):
+        new_tail = Node(new_value)
+        current_tail = self.tail_node
+
+        if current_tail is not None:
+          current_tail.set_next_node(new_tail)
+          new_tail.set_prev_node(current_tail)
+
+        self.tail_node = new_tail
+
+        if self.head_node is None:
+          self.head_node = new_tail
+
+     def remove_head(self):
+        removed_head = self.head_node
+
+        if removed_head is None:
+          return None
+
+        self.head_node = removed_head.get_next_node()
+
+        if self.head_node is not None:
+          self.head_node.set_prev_node(None)
+
+        if removed_head is self.tail_node:
+          self.remove_tail()
+
+        return removed_head.get_value()
+
+    def remove_tail(self):
+        removed_tail = self.tail_node
+
+        if removed_tail is None:
+          return None
+
+        self.tail_node = removed_tail.get_prev_node()
+        if self.tail_node is not None:
+          self.tail_node.set_next_node(None)
+
+        if removed_tail is self.head_node:
+          self.remove_head()
+
+        return removed_tail.get_value()
+
+    def remove_by_value(self, value_to_remove):
+        node_to_remove = None
+        current_node = self.head_node
+
+        while current_node is not None:
+            if current_node.get_value() is value_to_remove:
+                node_to_remove = current_node
+                break
+            current_node = current_node.get_next_node()
+
+        if node_to_remove is None:
+            return None
+
+        if node_to_remove is self.head_node:
+            self.remove_head()
+        elif node_to_remove is self.tail_node:
+            self.remove_tail()
+        else:
+            next_node = node_to_remove.get_next_node()
+            prev_node = node_to_remove.get_prev_node()
+            next_node.set_prev_node(prev_node)
+            prev_node.set_next_node(next_node)
+
+        return node_to_remove
+
+    def stringify_list(self):
+    string_list = ""
+    current_node = self.head_node
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+
+
+## Playing around with a subway excersize:
+
+subway = DoublyLinkedList()
+subway.add_to_head("Times Square")
+subway.add_to_head("Grand Central")
+subway.add_to_head("Central Park")
+print(subway.stringify_list())
+subway.add_to_tail("Penn Station")
+subway.add_to_tail("Wall Street")
+subway.add_to_tail("Brooklyn Bridge")
+print(subway.stringify_list())
+subway.remove_head()
+subway.remove_tail()
+print(subway.stringify_list())
+subway.remove_by_value("Times Square")
+print(subway.stringify_list())
