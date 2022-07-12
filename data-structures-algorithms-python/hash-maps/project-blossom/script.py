b58a03388eb787d018685c7d600a86ad4ac5aca6
Dan Hardej <danielhardej@gmail.com>
2022-07-12 10:27:11 +0100

    Hash Maps project

---
+++ data-structures-algorithms-python/hash-maps/project-blossom/script.py
@@ -0,0 +1,51 @@
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
+
+class HashMap:
+  def __init__(self, size):
+    self.array_size = size
+    # self.array = [None for item in range(size)]
+    self.array = [LinkedList() for item in range(size)]
+
+  def hash(self, key):
+    return sum(key.encode())
+
+  def compress(self, hash_code):
+    return hash_code % self.array_size
+
+  def assign(self, key, value):
+    array_index = self.compress(self.hash(key))
+    list_at_array = self.array[array_index]
+    payload = Node([key, value])
+    for item in list_at_array:
+      if item[0] == key:
+        item[1] = value
+        return
+    list_at_array.insert(payload)
+
+
+  def retrieve(self, key):
+    array_index = self.compress(self.hash(key))
+
+    list_at_index = self.array[array_index]
+
+    for item in list_at_index:
+      if item[0] == key:
+        return item[1]
+      else:
+        return None
+
+    # if payload is not None:
+    #   if payload[0] == key:
+    #     return payload[1]
+    # if payload is None:
+    #   return None
+    # if payload[0] != key:
+    #   return None
+
+blossom = HashMap(len(flower_definitions))
+
+for flower in flower_definitions:
+  blossom.assign(flower[0], flower[1])
+
+print(blossom.retrieve('daisy'))
