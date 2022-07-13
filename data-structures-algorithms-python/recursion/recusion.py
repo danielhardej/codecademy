70b0e79e90e40408568b5c61525ac75e3195e45d
Dan Hardej <danielhardej@gmail.com>
2022-07-13 09:51:34 +0100

    Update recusion.py

--- data-structures-algorithms-python/recursion/recusion.py
+++ data-structures-algorithms-python/recursion/recusion.py
@@ -31,3 +31,18 @@ def flatten(my_list):
 
 planets = ['mercury', 'venus', ['earth'], 'mars', [['jupiter', 'saturn']], 'uranus', ['neptune', 'pluto']]
 print(flatten(planets))
+
+
+# define the fibonacci() function below...
+def fibonacci(n):
+  if n == 1:
+    return 1
+  if n == 0:
+    return 0
+  print("Recursive call with {0} as input".format(n))
+  return fibonacci(n-1) + fibonacci(n-2)
+
+fibonacci(5)
+# set the appropriate runtime:
+# 1, logN, N, N^2, 2^N, N!
+fibonacci_runtime = "2^N"
