cfde27be1cb27d4dc93b56dadc250b186eabbfdc
Dan Hardej <danielhardej@gmail.com>
2022-07-13 15:37:29 +0100

    Update recusion.py

--- data-structures-algorithms-python/recursion/recusion.py
+++ data-structures-algorithms-python/recursion/recusion.py
@@ -46,3 +46,27 @@ fibonacci(5)
 # set the appropriate runtime:
 # 1, logN, N, N^2, 2^N, N!
 fibonacci_runtime = "2^N"
+
+
+
+# Define build_bst() below...
+def build_bst(my_list):
+  if len(my_list) == 0:
+    return "No Child"
+  middle_idx = len(my_list) // 2
+  print("Middle index: {}".format(middle_idx))
+  middle_value = my_list[middle_idx]
+  print("Middle value: {}".format(middle_value))
+  tree_node = {"data" : middle_value}
+  tree_node["left_child"] = build_bst(my_list[0:middle_idx])
+  tree_node["right_child"] = build_bst(my_list[middle_idx+1:])
+  return tree_node
+
+# For testing
+sorted_list = [12, 13, 14, 15, 16]
+binary_search_tree = build_bst(sorted_list)
+print(binary_search_tree)
+
+# fill in the runtime as a string
+# 1, logN, N, N*logN, N^2, 2^N, N!
+runtime = "N*logN"
