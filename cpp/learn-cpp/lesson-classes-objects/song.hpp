153d5263b1a288e943d6060405d6e2b43c4e5724
Dan Hardej <danielhardej@gmail.com>
2022-10-25 11:23:03 +0800

    Lesson on C++ classes and objects complete

---
+++ cpp/learn-cpp/lesson-classes-objects/song.hpp
@@ -0,0 +1,17 @@
+#include <string>
+
+class Song {
+
+  std::string title;
+  std::string artist;
+
+public:
+  Song(std::string new_title, std::string new_artist);
+  // Add a destructor here:
+  ~Song();
+
+  std::string get_title();
+
+  std::string get_artist();
+
+};
