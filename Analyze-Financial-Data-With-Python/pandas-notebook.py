66b6cd43f04a64e8c0566240286605b9334a2900
Dan Hardej <danielhardej@gmail.com>
2023-03-12 15:29:00 +0800

    Create pandas-notebook.py

---
+++ Analyze-Financial-Data-With-Python/pandas-notebook.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+
+shoe_counts = orders.groupby(['shoe_type', 'shoe_color']).id.count().reset_index()
+
+# compare purchases of different shoe colors of the same shoe type by creating a pivot table
+shoe_counts_pivot = shoe_counts.pivot(
+  columns = 'shoe_color',
+  index = 'shoe_type',
+  values = 'id'
+).reset_index()
