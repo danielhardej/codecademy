c32979cacc3ce4ce1c2cb81d3cc9c6492905dbfc
Dan Hardej <danielhardej@github.com>
2023-04-08 16:56:48 +0800

    Reorganize files

---
+++ Analyze-Financial-Data-With-Python/4-Visualizing-Finance-Data/Portfolio-Optimization/Mean-Variance-Portfolio-Optimization-Project/op.py
@@ -0,0 +1,33 @@
+import cvxopt as opt
+from cvxopt import blas, solvers
+import numpy as np
+
+def optimal_portfolio(returns):
+    n = returns.shape[1]
+    returns = np.transpose(returns.values)
+
+    N = 100
+    mus = [10**(5.0 * t/N - 1.0) for t in range(N)]
+
+    # Convert to cvxopt matrices
+    S = opt.matrix(np.cov(returns))
+    pbar = opt.matrix(np.mean(returns, axis=1))
+
+    # Create constraint matrices
+    G = -opt.matrix(np.eye(n))   # negative n x n identity matrix
+    h = opt.matrix(0.0, (n ,1))
+    A = opt.matrix(1.0, (1, n))
+    b = opt.matrix(1.0)
+
+    # Calculate efficient frontier weights using quadratic programming
+    portfolios = [solvers.qp(mu*S, -pbar, G, h, A, b)['x']
+                  for mu in mus]
+    ## CALCULATE RISKS AND RETURNS FOR FRONTIER
+    returns = [blas.dot(pbar, x) for x in portfolios]
+    risks = [np.sqrt(blas.dot(x, S*x)) for x in portfolios]
+    ## CALCULATE THE 2ND DEGREE POLYNOMIAL OF THE FRONTIER CURVE
+    m1 = np.polyfit(returns, risks, 2)
+    x1 = np.sqrt(m1[2] / m1[0])
+    # CALCULATE THE OPTIMAL PORTFOLIO
+    wt = solvers.qp(opt.matrix(x1 * S), -pbar, G, h, A, b)['x']
+    return np.asarray(wt), returns, risks
